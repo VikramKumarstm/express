d3ae03a5fc0b58066a055c5156b34011766260da
VikramKumarstm <vikramclg0000@gmail.com>
2025-11-28 08:19:21 +0530

    get user data from database

--- CRUD/src/controllers/userController.js
+++ CRUD/src/controllers/userController.js
@@ -10,6 +10,16 @@ export const createUser = async (req, res) => {
 
         const { name, email } = req.body;
 
+        if (!name || !email) {
+            throw new Error('Name and Email are required')
+        }
+
+        const userExists = await User.findOne({email});
+
+        if (userExists) {
+            throw new Error('User already exists.')
+        }
+
         const user = await User.create({
             name,
             email
@@ -27,5 +37,26 @@ export const createUser = async (req, res) => {
             success: false,
             message: error.message,
         })
+    }
+}
+
+export const getUsers = async (req, res) => {
+    try {
+
+        const users = await User.find({});
+
+        res.status(200).json({
+            success: true,
+            message: 'Data downloaded successfully',
+            users
+        })
+        
+    } catch (error) {
+        console.error("ERROR", error);
+        res.status(400).json({
+            success: false,
+            message: error.message,
+        })
+        
     }
 }
--- CRUD/src/routes/userRoutes.js
+++ CRUD/src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { home, createUser, } from '../controllers/userController.js'
+import { home, createUser, getUsers, } from '../controllers/userController.js'
 
 const router = express.Router();
 
 router.get('/', home)
 router.post('/createUser', createUser)
+router.get('/getUsers', getUsers)
 export default router
